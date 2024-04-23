e9ee68463feb35427b7b74fb1a0b97f722d2b1da
rabiakhan01 <rabiakham789@gmail.com>
2024-04-23 17:16:21 +0500

    feat: Optimised routes defines

--- src/App.js
+++ src/App.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, RouterProvider, createBrowserRouter, Navigate } from "react-router-dom";
 import Login from './pages/Login/index.js';
 import AddUser from './pages/AddUser/index.js';
 import Listing from './pages/Listing/index.js';
 import SignUp from './pages/Signup/index.js';
 import PageNotFound from './pages/PageNotFound/index.js';
-
 import './index.js';
+import { isLoginUser } from './utils/utils.js';
 
 function App() {
+
+  const ProtectedRoute = ({ children }) => {
+
+    return isLoginUser() ? children : <Navigate to="/" />
+
+  }
+
+  const PublicRoute = ({ children }) => {
+
+    return !isLoginUser() ? children : <Navigate to="/student-listing" />
+
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <PublicRoute><Login /></PublicRoute>,
+    },
+    {
+      path: '/signup',
+      element: <PublicRoute><SignUp /></PublicRoute>
+    },
+    {
+      path: '/student-listing',
+      element: <ProtectedRoute><Listing /></ProtectedRoute>,
+    },
+    {
+      path: '/add-new-student',
+      element: <ProtectedRoute><AddUser /></ProtectedRoute>,
+    },
+    {
+      path: '/update-student/:id',
+      element: <ProtectedRoute><AddUser /></ProtectedRoute>
+    }
+  ])
+
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<SignUp />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/add-new-student' element={<AddUser />} />
-        <Route path='/student-listing' element={<Listing />} />
-        <Route path='/update-student/:id' element={<AddUser />} />
-        <Route path='*' element={<PageNotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   );
 }
 
 export default App;
+
+
+
+
